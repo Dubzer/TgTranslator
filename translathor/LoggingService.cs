c1969149711fcf180ef73ae7fe83cf0bfb6f3f91
Dubzer <Dubzer@users.noreply.github.com>
2019-06-06 02:42:42 +0300

    Fixed all messages

--- translathor/LoggingService.cs
+++ translathor/LoggingService.cs
@@ -5,8 +5,8 @@ namespace Translathor
 {
     public class LoggingService
     {
-        private static string logDirectory { get; set; }
-        private static string logFile => Path.Combine(logDirectory, $"{DateTime.UtcNow.ToString("yyyy-MM-dd")}.txt");
+        private static string logDirectory;
+        private static string LogFile => Path.Combine(logDirectory, $"{DateTime.UtcNow.ToString("yyyy-MM-dd")}.txt");
 
         public static void PrepareDirectory()
         {
@@ -15,14 +15,14 @@ namespace Translathor
 
             if (!Directory.Exists(logDirectory))     // Create the log directory if it doesn't exist
                 Directory.CreateDirectory(logDirectory);
-            if (!File.Exists(logFile))               // Create today's log file if it doesn't exist
-                File.Create(logFile).Dispose();
+            if (!File.Exists(LogFile))               // Create today's log file if it doesn't exist
+                File.Create(LogFile).Dispose();
         }
 
         public static void Log(string message)
         {
             string logText = DateTime.Now.ToString("[HH:mm:ss] ") + message;
-            File.AppendAllText(logFile, logText + "\n");     // Write the log text to a file
+            File.AppendAllText(LogFile, logText + "\n");     // Write the log text to a file
 
             Console.Out.WriteLine(logText);       // Write the log text to the console
         }
