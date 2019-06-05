11295f941e2e4a240a9fea51ee56f85f7d67bc12
Dubzer <Dubzer@users.noreply.github.com>
2019-06-06 02:40:50 +0300

    Fixed entry point

--- translathor/Program.cs
+++ translathor/Program.cs
@@ -11,7 +11,7 @@ namespace Translathor
 
         public static IConfigurationRoot Configuration { get; set; }
 
-        static async void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var builder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile("configuration.json");
             Configuration = builder.Build();
