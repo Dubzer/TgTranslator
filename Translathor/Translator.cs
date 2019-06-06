d67c32f8a3d1b861af0308ebc3ca205398522078
Dubzer <Dubzer@users.noreply.github.com>
2019-06-06 15:31:31 +0300

    Splitted UpdateHandler on 2 classes

---
+++ Translathor/Translator.cs
@@ -0,0 +1,38 @@
+﻿using Extensions;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using YandexTranslateCSharpSdk;
+
+namespace Translathor
+{
+    class Translator
+    {
+        YandexTranslateSdk yaTranslator = new YandexTranslateSdk();
+
+        public Translator(string apiKey)
+        {
+            yaTranslator.ApiKey = apiKey;
+        }
+
+        public async Task<string> DetectLanguage(string text)
+        {
+            // Text without English symbols
+            string textWOEng = Regex.Replace(text.WithoutLinks(), @"[A-Za-z0-9 .,-=@+(){}\[\]\\]", "");
+
+            if (!string.IsNullOrWhiteSpace(textWOEng))
+            {
+                return await yaTranslator.DetectLanguage(textWOEng);
+            }
+
+            return await yaTranslator.DetectLanguage(text.WithoutLinks());
+        }
+
+        public async Task<string> TranslateText(string text, string from, string to)
+        {
+            string translation = await yaTranslator.TranslateText(text, $"{from}-{to}");
+            LoggingService.Log($"Translated {text} ({from}) to {translation} ({to})");
+
+            return translation;
+        }
+    }
+}
