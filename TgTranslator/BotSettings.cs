ad60ddd9bdb43efcc608482fdf1b3531c3320db7
Dubzer <Dubzer@users.noreply.github.com>
2019-06-08 02:26:03 +0300

    +Method to switch item in settings menu

--- TgTranslator/BotSettings.cs
+++ TgTranslator/BotSettings.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Telegram.Bot.Args;
+using TgTranslator.Settings;
 
 namespace TgTranslator
 {
     class BotSettings
     {
+        List<Setting> settings;
 
         public BotSettings()
         {
+            settings = new List<Setting>()
+            {
+                new Language("Language")
+            };
+        }
 
+        void SwitchItem(MessageEventArgs e, Setting item)
+        {
+            Program.botClient.EditMessageCaptionAsync(e.Message.Chat.Id, e.Message.From.Id, item.message, item.GenerateButtons());
         }
     }
 }
