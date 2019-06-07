fb3e941217dab9bc2bf0ef8513717c07d70a82e3
Dubzer <Dubzer@users.noreply.github.com>
2019-06-07 23:08:56 +0300

    Begin implementation of settings

---
+++ Translathor/Settings/Setting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Translathor.Settings
+{
+    abstract class Setting
+    {
+        protected virtual InlineKeyboardMarkup GenerateButtons()
+        {
+            List<InlineKeyboardButton> buttons = new List<InlineKeyboardButton>();
+            buttons.Add(new InlineKeyboardButton { })
+            return new InlineKeyboardMarkup(buttons);
+        }
+    }
+}
