38d329cab598275b281212d4bed3b226f2ff0208
Dubzer <Dubzer@users.noreply.github.com>
2019-06-10 01:18:38 +0300

    Implemented ApplyMenu...
    - Working on Language setting
    - Improved architecture

--- TgTranslator/Settings/Language.cs
+++ TgTranslator/Settings/Language.cs
@@ -1,16 +1,24 @@
-﻿namespace TgTranslator.Settings
+﻿using System.Collections.Generic;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TgTranslator.Settings
 {
     class Language : Setting
     {
         public Language(string itemTitle)
         {
             this.itemTitle = itemTitle;
-            command = "/switchlang ";
+            command = "/switchlang=";
             description = "Here you can setup primary language for your group. If you don't see your language, select **Other**";
         }
 
         public override void GenerateButtons()
         {
+            //buttons.Add(new List<InlineKeyboardButton>() { new InlineKeyboardButton { Text = "🇬🇧 English", CallbackData = $"ApplyMenu {command}en" }, new InlineKeyboardButton { Text = "🇪🇸 Spanish", CallbackData = $"ApplyMenu {command}es" } });
+            //buttons.Add(new List<InlineKeyboardButton>() { new InlineKeyboardButton { Text = "🇮🇷 Persian", CallbackData = $"ApplyMenu {command}fa" }, new InlineKeyboardButton { Text = "🇷🇺 Russian", CallbackData = $"ApplyMenu {command}ru" } });
+            //buttons.Add(new List<InlineKeyboardButton>() { new InlineKeyboardButton { Text = "🇮🇷 Persian", CallbackData = $"ApplyMenu {command}fa" }, new InlineKeyboardButton { Text = "🇷🇺 Russian", CallbackData = $"ApplyMenu {command}ru" } });
+            buttons.Add(new List<InlineKeyboardButton>() { new InlineKeyboardButton { Text = "🇮🇷 Persian", CallbackData = $"ApplyMenu {command}fa" }, new InlineKeyboardButton { Text = "🇷🇺 Russian", CallbackData = $"ApplyMenu {command}ru" } , new InlineKeyboardButton { Text = "🇷🇺 Russian", CallbackData = $"ApplyMenu {command}ru" } });
+
             base.GenerateButtons();
         }
     }
--- TgTranslator/Settings/MainMenu.cs
+++ TgTranslator/Settings/MainMenu.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TgTranslator.Settings
@@ -15,14 +13,12 @@ namespace TgTranslator.Settings
 
         public override void GenerateButtons()
         {
-            buttons = new List<InlineKeyboardButton>();
-
             foreach (var setting in BotSettings.settings)
             {
                 if (setting.GetType().ToString() == "TgTranslator.Settings.MainMenu")
                     continue;
 
-                buttons.Add(new InlineKeyboardButton { Text = setting.itemTitle, CallbackData = setting.GetType().ToString() });
+                buttons.Add(new List<InlineKeyboardButton>() { new InlineKeyboardButton { Text = setting.itemTitle, CallbackData = setting.GetType().ToString() } });
             }
 
         }
