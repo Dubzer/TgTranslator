a86b57f53a49ee2e80d92cd1e8d756dacb17e529
Dubzer <Dubzer@users.noreply.github.com>
2019-06-22 20:54:17 +0300

    Large refactoring

--- TgTranslator/Settings/MainMenu.cs
+++ TgTranslator/Settings/MainMenu.cs
@@ -5,22 +5,28 @@ namespace TgTranslator.Settings
 {
     class MainMenu : Setting
     {
+        
         public MainMenu(string itemTitle)
         {
             this.itemTitle = itemTitle;
             description = "Some text that says something about settings";
         }
 
-        protected override void GenerateButtons()
+        private void GenerateButtons(IEnumerable<Setting> settingsList)
         {
-            foreach (var setting in BotSettings.settings)
+            foreach (var setting in settingsList)
             {
                 if (setting.GetType().ToString() == "TgTranslator.Settings.MainMenu")
                     continue;
 
                 buttons.Add(new List<InlineKeyboardButton> { new InlineKeyboardButton { Text = setting.itemTitle, CallbackData = setting.GetType().ToString() } });
             }
+        }
 
+        public InlineKeyboardMarkup GenerateMarkup(IEnumerable<Setting> settingsList)
+        {
+            GenerateButtons(settingsList);
+            return GenerateMarkup();
         }
     }
 }
