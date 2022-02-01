b80883dc12e0354f78bb00183d0db36b6eb32b32
Dubzer <dubzer@ya.ru>
2022-02-02 01:19:55 +0300

    Migrate to INT64

---
+++ src/Migrations/20220129132308_int64.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TgTranslator.Migrations
+{
+    public partial class int64 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "user_id",
+                table: "users",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "user_id",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
