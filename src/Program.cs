bf435d682ae732f39de6b7b30c2ae7e93bcc4db4
Dubzer <dubzer@ya.ru>
2022-02-03 16:03:43 +0300

    Add Sentry

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Sentry;
 using Serilog;
 using TgTranslator.Data.Options;
 using TgTranslator.Extensions;
@@ -17,6 +18,16 @@ public static class Program
     public static string Username;
     public static async Task Main()
     {
+        SentrySdk.Init(o =>
+        {
+            o.Dsn = "https://380e0a36b482415cabd1fd621c1a030d@o797589.ingest.sentry.io/6181857";
+            // When configuring for the first time, to see what the SDK is doing:
+            o.Debug = false;
+            // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            o.TracesSampleRate = 1.0;
+        });
+
         CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
         CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
         AppDomain.CurrentDomain.UnhandledException += (_, eventArgs) => Log.Fatal(eventArgs.ExceptionObject as Exception, "Something went terribly wrong");
@@ -24,6 +35,8 @@ public static class Program
         await CreateHostBuilder()
             .Build()
             .RunAsync();
+        
+        SentrySdk.Close();
     }
 
     private static IHostBuilder CreateHostBuilder() =>
