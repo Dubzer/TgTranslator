0b7c54755299e1c026a50b51991bc2b179d5ba82
Dubzer <me@dubzer.dev>
2023-07-05 02:15:36 +0300

    Minor change for splitter again

--- src/Utils/MessageSplitter.cs
+++ src/Utils/MessageSplitter.cs
@@ -6,18 +6,16 @@ public static class MessageSplitter
 {
     private static readonly char[] SplitChars = {'.', '?', '!', ','};
 
-    public static (string, string) Split(ReadOnlySpan<char> message)
+    public static (string firstPart, string secondPart) Split(ReadOnlySpan<char> message)
     {
-        // We use ..4096 instead of ..4097 because we want char to be included in the first part
+        // We add 1 because we want char to be included in the first part of the translation
+        // Hence we don't include 4096th char
         var lastEndOfSentence = message[..4096].LastIndexOfAny(SplitChars) + 1;
 
         // Hard trim
         if(lastEndOfSentence == 0)
             lastEndOfSentence = 4096;
 
-        var firstPart = message[..lastEndOfSentence];
-        var secondPart = message[lastEndOfSentence..];
-
-        return (firstPart.ToString(), secondPart.ToString());
+        return (message[..lastEndOfSentence].ToString(), message[lastEndOfSentence..].ToString());
     }
 }
