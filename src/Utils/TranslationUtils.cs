e9e6f3066411f92d66d9f1fdde5426511c3ee96b
Dubzer <me@dubzer.dev>
2023-07-22 04:14:18 +0300

    Fix mentions

--- src/Utils/TranslationUtils.cs
+++ src/Utils/TranslationUtils.cs
@@ -7,14 +7,14 @@ namespace TgTranslator.Utils;
 
 public static class TranslationUtils
 {
-    public static string FixEntities(string translation, MessageEntity[]? entities)
+    public static string FixEntities(string messageText, string translation, MessageEntity[]? entities)
     {
         if (entities == null || entities.Length == 0)
             return translation;
 
         var usernames = entities
-            .Where(x => x is { Type: MessageEntityType.Mention, User.Username: not null })
-            .Select(x => x.User!.Username![1..])
+            .Where(x => x is { Type: MessageEntityType.Mention })
+            .Select(x => messageText[(x.Offset + 1)..(x.Offset + x.Length)])
             .ToArray();
 
         if (usernames.Length == 0)
