5a6ec9fea4499503f862921a1f02477e9bc16fbd
Dubzer <me@dubzer.dev>
2023-07-23 03:26:53 +0300

    Remove interface

--- src/Interfaces/IMetrics.cs
+++
@@ -1,9 +0,0 @@
-namespace TgTranslator.Interfaces;
-
-public interface IMetrics
-{
-    void HandleGroupMessage(long groupId, int charactersCount);
-    void HandleTranslatorApiCall(long groupId, int charactersCount);
-    void HandleMessage();
-    void HandleGroupsCountUpdate(int count);
-}
