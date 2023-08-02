d92d64668c78b20a324528e2a24cd23ca8de62e2
Dubzer <me@dubzer.dev>
2023-08-02 13:25:39 +0300

    Add missing using

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Sentry;
 using Serilog;
 using TgTranslator.Data.Options;
