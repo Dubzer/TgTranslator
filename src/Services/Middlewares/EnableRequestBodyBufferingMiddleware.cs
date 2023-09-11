5a1b0e64a0c5d100f55f815f71a2c894b5385236
Dubzer <me@dubzer.dev>
2023-09-11 22:22:43 +0300

    Force using Newtonsoft.Json for Telegram updates controller since lib relies on it

---
+++ src/Services/Middlewares/EnableRequestBodyBufferingMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TgTranslator.Services.Middlewares;
+
+public class EnableRequestBodyBufferingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public EnableRequestBodyBufferingMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Request.EnableBuffering();
+        await _next(context);
+    }
+}
---
+++ src/Utils/Extensions/AspNetCodeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TgTranslator.Utils.Extensions;
+
+public static class AspNetCodeExtensions
+{
+    // https://markb.uk/asp-net-core-read-raw-request-body-as-string.html
+    public static async Task<string> GetRawBodyAsync(
+        this HttpRequest request,
+        Encoding encoding = null)
+    {
+        if (!request.Body.CanSeek)
+            request.EnableBuffering();
+
+        request.Body.Position = 0;
+
+        var reader = new StreamReader(request.Body, encoding ?? Encoding.UTF8);
+        var body = await reader.ReadToEndAsync();
+        request.Body.Position = 0;
+
+        return body;
+    }
+}
